9ab65d1477317c55a86c438c0c5f38b87d12fdd8
RohanSha05 <rhnshariar@gmail.com>
2022-03-02 22:33:03 +0600

    final commit

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,4 @@
+// -------------Search Phone 
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -8,7 +9,7 @@ const searchPhone = () => {
         .then(res => res.json())
         .then(data => displaySearchResults(data.data));
 }
-
+// -----------Display Search Result 
 const displaySearchResults = (phones) => {
     const searchResult = document.getElementById('search-result')
     console.log('ok')
@@ -40,39 +41,32 @@ const displaySearchResults = (phones) => {
         }
     }
 }
-
+//------------------------------- Phone Details --------------------------
 const searchDetails = (phoneId) => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhoneDetails(data.data))
 }
-// console.log(data.data.mainFeatures.storage)
+// ------------------Display Details--------------
 const displayPhoneDetails = (phoneDetails) => {
     const phoneDetailsField = document.getElementById('phone-details-field');
+    phoneDetailsField.innerHTML = '';
     const div = document.createElement('div')
-    div.classList.add('col')
-    // div.innerHTML = '';
-    /* let releaseDate = phoneDetails.releaseDate
-    console.log(releaseDate);
-    if (releaseDate.length == 0) {
-        releaseDate = 'Not Released Yet'
-    }
-    else {
-        return releaseDate;
-    } */
+    div.classList.add('col', 'shadow-lg', 'w-75')
     div.innerHTML = `
-    <div class=" d-flex justify-content-center m-5">
-                <div class="d-flex align-items-center"><img src="${phoneDetails.image}" class=" mx-auto" alt="..."></div>
+    <div class=" d-flex justify-content-center m-5 ">
+                <div class="d-flex align-items-center "><img src="${phoneDetails.image}" class=" mx-auto" alt="..."></div>
             <div class="">
                 <h5 class="card-title text-center">Model: ${phoneDetails.name}</h5>
                 <p class="card-text text-center">Brand:${phoneDetails.brand}</p>
+                <h5 class="card-title text-center">Main Features</h5>
                 <p class="card-text ms-3">Storage:${phoneDetails.mainFeatures.storage}</p>
                 <p class="card-text ms-3">Display:${phoneDetails.mainFeatures.displaySize}</p>
                 <p class="card-text ms-3">Chipset:${phoneDetails.mainFeatures.chipSet}</p>
                 <p class="card-text ms-3">Memory:${phoneDetails.mainFeatures.memory}</p>
                 <p class="card-text ms-3">Sensors:${phoneDetails.mainFeatures.sensors}</p>
-                <p class="card-text ms-3">ReleaseDate: ${phoneDetails.releaseDate ? phoneDetails.releaseDate : ' Not released Yet'}</p>
+                <p class="card-text ms-3">Release Date: ${phoneDetails.releaseDate ? phoneDetails.releaseDate : ' Not released Yet'}</p>
                 <p class="card-text ms-3">Others:</p>
                 <p class="card-text ms-3">WLAN:${phoneDetails.others?.WLAN}</p>
                 <p class="card-text ms-3">Bluetooth:${phoneDetails.others?.Bluetooth}</p>
